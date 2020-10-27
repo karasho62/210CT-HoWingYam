c6679afe725ded43f5d28a83ca1b94f577e3c4ac
karasho62 <karasho62@gmail.com>
2020-10-27 19:38:42 +0800

    updateTest 03

--- src/BasicW1Q1/BasicW1Q1.java
+++ src/BasicW1Q1/BasicW1Q1.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 class BasicW1Q1 {
 
-    private static void main(String args[]) {
+    public static void main(String args[]) {
         try {
             int testing002=0;
             Scanner sc = new Scanner(System.in);
